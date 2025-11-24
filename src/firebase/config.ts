864cd6efe87c7d67aff2629be80f5c0fdeff97d1
lkhagvasuren Otgonbayar <grafx88104099@gmail.com>
2025-11-24 11:32:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3377746481-9f94d",
+  "appId": "1:215024628960:web:54f50d55998a56aafacc85",
+  "apiKey": "AIzaSyD3lCQMWpLPXvgRcofT1Q8GZbUS9dk1RcU",
+  "authDomain": "studio-3377746481-9f94d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "215024628960"
+};
