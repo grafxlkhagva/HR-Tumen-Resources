3c0ca2638b71972481ee734daa915006ec269855
lkhagvasuren Otgonbayar <grafx88104099@gmail.com>
2025-12-08 07:54:29 +0000

    Error: React.Children.only expected to receive a single React element ch

--- src/app/main-nav.tsx
+++ src/app/main-nav.tsx
@@ -14,6 +14,7 @@ import {
   Award,
   MessageSquare,
   Sparkles,
+  Gift,
 } from 'lucide-react';
 import {
   SidebarMenu,
@@ -41,6 +42,11 @@ const links = [
     href: '/dashboard/employees',
     label: 'Ажилтан',
     icon: Users,
+  },
+  {
+    href: '/dashboard/points',
+    label: 'Оноо шилжүүлэх',
+    icon: Gift,
   },
   {
     href: '/dashboard/attendance',
