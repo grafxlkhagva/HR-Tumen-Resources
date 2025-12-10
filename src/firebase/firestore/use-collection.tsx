bb07c0b326d5708d81e3d632e7887740fb324d96
lkhagvasuren Otgonbayar <grafx88104099@gmail.com>
2025-12-10 11:38:35 +0000

    энэ заавра туслах болов уу энэ алдааг шийдэгэхэд Fix insecure rules
    boo

--- src/firebase/firestore/use-collection.tsx
+++ src/firebase/firestore/use-collection.tsx
@@ -26,18 +26,6 @@ export interface UseCollectionResult<T> {
   error: FirestoreError | Error | null; // Error object, or null.
 }
 
-/* Internal implementation of Query:
-  https://github.com/firebase/firebase-js-sdk/blob/c5f08a9bc5da0d2b0207802c972d53724ccef055/packages/firestore/src/lite-api/reference.ts#L143
-*/
-export interface InternalQuery extends Query<DocumentData> {
-  _query: {
-    path: {
-      canonicalString(): string;
-      toString(): string;
-    }
-  }
-}
-
 /**
  * React hook to subscribe to a Firestore collection or query in real-time.
  * Handles nullable references/queries.
@@ -63,12 +51,10 @@ export function useCollection<T = any>(
       setIsLoading(false);
       setData(null);
       setError(null);
-      return () => {}; // Return an empty cleanup function
+      return () => {};
     }
 
     setIsLoading(true);
-    setError(null);
-    setData(null);
 
     const unsubscribe = onSnapshot(
       refOrQuery,
