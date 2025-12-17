ee8b42c681971230eada1fb15e8f11aa84f8b1d1
lkhagvasuren Otgonbayar <grafx88104099@gmail.com>
2025-12-17 04:46:05 +0000

    асуудал хэвээрээ

--- src/app/dashboard/organization/add-position-dialog.tsx
+++ src/app/dashboard/organization/add-position-dialog.tsx
@@ -217,8 +217,8 @@ export function AddPositionDialog({
           </DialogDescription>
         </DialogHeader>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1 min-h-0 flex flex-col gap-4">
-            <ScrollArea className="flex-grow pr-4 -mr-4">
+          <form onSubmit={form.handleSubmit(onSubmit)} className="flex-grow flex flex-col min-h-0">
+            <ScrollArea className="flex-grow pr-6 -mr-6">
               <div className="space-y-4 py-1">
                 <FormField
                   control={form.control}
@@ -473,7 +473,7 @@ export function AddPositionDialog({
                 />
               </div>
             </ScrollArea>
-            <DialogFooter className="pt-4 border-t">
+            <DialogFooter className="pt-4 border-t mt-4 flex-shrink-0">
               <Button type="button" variant="outline" onClick={() => onOpenChange(false)} disabled={isSubmitting}>
                 Цуцлах
               </Button>
