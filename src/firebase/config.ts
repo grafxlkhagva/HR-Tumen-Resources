a8c3cae36749ab24e6b44f4f2bdbf8b8cb23fe99
huhenege <huhenege@huheneges-MacBook-Pro.local>
2026-01-22 18:01:13 +0800

    feat: Firebase төслийг tumen-resourse-hr рүү шилжүүлэх

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  "projectId": "YOUR_NEW_PROJECT_ID",
-  "appId": "YOUR_NEW_APP_ID",
-  "apiKey": "YOUR_NEW_API_KEY",
-  "authDomain": "YOUR_NEW_PROJECT_ID.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "YOUR_NEW_MESSAGING_SENDER_ID",
-  "storageBucket": "YOUR_NEW_PROJECT_ID.firebasestorage.app"
+  "projectId": "tumen-resourse-hr",
+  "appId": "1:12931846642:web:f46b64e956850fac1cdb6b",
+  "apiKey": "AIzaSyDG_ql_OSX4mDyFRtRODMcpy6wyUi-o7zA",
+  "authDomain": "tumen-resourse-hr.firebaseapp.com",
+  "measurementId": "G-7CYVDE01DN",
+  "messagingSenderId": "12931846642",
+  "storageBucket": "tumen-resourse-hr.firebasestorage.app"
 };
 
