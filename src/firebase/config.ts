0ba3310a7b498601456c592e1b7a95575a36f48b
huhenege <huhenege@huheneges-MacBook-Pro.local>
2026-01-22 18:00:29 +0800

    refactor: Firebase төслийн тохиргоог шинэчлэх template

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  "projectId": "studio-3377746481-9f94d",
-  "appId": "1:215024628960:web:54f50d55998a56aafacc85",
-  "apiKey": "AIzaSyD3lCQMWpLPXvgRcofT1Q8GZbUS9dk1RcU",
-  "authDomain": "studio-3377746481-9f94d.firebaseapp.com",
+  "projectId": "YOUR_NEW_PROJECT_ID",
+  "appId": "YOUR_NEW_APP_ID",
+  "apiKey": "YOUR_NEW_API_KEY",
+  "authDomain": "YOUR_NEW_PROJECT_ID.firebaseapp.com",
   "measurementId": "",
-  "messagingSenderId": "215024628960",
-  "storageBucket": "studio-3377746481-9f94d.firebasestorage.app"
+  "messagingSenderId": "YOUR_NEW_MESSAGING_SENDER_ID",
+  "storageBucket": "YOUR_NEW_PROJECT_ID.firebasestorage.app"
 };
 
